f796a6aebccfbc668f578e6a496b356219c644c7
Linas Kriščiukaitis <l.krisciukaitis@gmail.com>
2021-03-27 18:07:49 +0200

    js

--- js/components/matematika.js
+++ js/components/matematika.js
@@ -1,1 +1,19 @@
-const 
+const sum = (a, b) => a + b;
+const minus = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const devide = (a, b) => a / b;
+
+const matematika = {
+    sudeti: sum,
+    atimti: minus,
+    dauginti: multiply,
+    dalinti: devide,
+    
+    sum: sum,
+    subtract: minus,
+    multiply: multiply,
+    devide: devide,
+
+}
+
+export { matematika };
---
+++ js/main-home.js
@@ -0,0 +1,19 @@
+// IMPORT
+
+// hero
+/* import { clock } from ''; /*
+/* import { form } from ''; */
+// main
+// footer
+
+
+// FUNKCIJU PANAUDOJIMAS
+
+// hero
+/* clock(); */
+/* form('subscribe');*/
+
+// main
+/* form('message'); */
+
+// footer
